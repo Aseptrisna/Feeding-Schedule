409c95c15f3df4a95389152148ff8db58b0d3002
Asep Trisna Setiawan <Aseptrisna1995@gmail.com>
2022-02-05 19:29:46 +0700

    set content

---
+++ app/src/main/java/com/project/feedingschedule/Features/Menu_Connect_Screen.java
@@ -0,0 +1,15 @@
+package com.project.feedingschedule.Features;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.project.feedingschedule.R;
+public class Menu_Connect_Screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_connect_screen);
+    }
+}
---
+++ app/src/main/java/com/project/feedingschedule/Features/Menu_Splash_Screen.java
@@ -0,0 +1,16 @@
+package com.project.feedingschedule.Features;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.project.feedingschedule.R;
+
+public class Menu_Splash_Screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_splash_screen);
+    }
+}
